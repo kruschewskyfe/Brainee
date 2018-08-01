a8f70bdef0eac02c141adcc4a9d42396593655d4
Cristian Roberto da Luz <contato@webmindwebstudio.onmicrosoft.com>
2018-08-01 17:20:12 -0300

    Implementando Modal

--- Brainee/index.php
+++ Brainee/index.php
@@ -50,7 +50,9 @@ Pensando nisso, a Brainee elaborou esse conteúdo. Então aproveite e leia até 
   <?php
     include ("grid-lateral.html")
   ?>
-
+<?php
+	include ("modal.html")
+?>
   <?php
     include ("footer.html")
   ?>
