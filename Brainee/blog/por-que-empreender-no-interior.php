4c4b8d7b8a88cde9a73c6ef001e675cc2208ecaa
Felipe Kruschewsky <37812972+kruschewskyfe@users.noreply.github.com>
2018-08-03 11:00:54 -0300

    Inserção do Sexto Post
    por-que-empreender-no-interior.php

---
+++ Brainee/blog/por-que-empreender-no-interior.php
@@ -0,0 +1,21 @@
+<?php
+	include ('../header.html')
+?>
+
+<?php
+    include ('sextoPost.html')
+?>
+
+<?php
+    include ('cta.html')
+?>
+
+ <?php
+    include ('../grid-lateral.html')
+  ?>
+<?php
+	include ("../modal.html")
+?>
+<?php
+    include ('../footer.html')
+?>
