37f9e4b84028c273f7e0deecdbe6ef4f5e885dad
Felipe Kruschewsky <37812972+kruschewskyfe@users.noreply.github.com>
2018-08-03 10:58:31 -0300

    Inserção do Quinto Post
    o-pitching-ideal.php

---
+++ Brainee/blog/o-pitching-ideal.php
@@ -0,0 +1,17 @@
+<?php
+	include ('../header.html')
+?>
+
+<?php
+    include ('quintoPost.html')
+?>
+
+ <?php
+    include ('../grid-lateral.html')
+  ?>
+<?php
+	include ("../modal.html")
+?>
+<?php
+    include ('../footer.html')
+?>
